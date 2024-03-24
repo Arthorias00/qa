737b856a793c52269c9a940fa3ce47fd2e7ccd53
Arthur-QA-000 <ibatullin.97@mail.ru>
2024-03-24 18:26:26 +0300

    решал задачку кодварс

--- PythonSintacsis/1.py
+++ PythonSintacsis/1.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+arr2 = ["trances", "nectar"]
+def letter_check(arr): 
+    xxx = "".join(set(sorted(str.lower(arr[0]))))
+    yyy = "".join(set(sorted(str.lower(arr[1]))))
+    gg = 0
+    while len(yyy) > gg:
+       if yyy[gg] in xxx:
+          gg+=1
+       else:
+            return False
+    return True
+        
+    
+print(letter_check(arr2))
