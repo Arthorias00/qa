ea64e94a2cefa250981a0dea980ffeba209ec4f7
Arthur-QA-000 <ibatullin.97@mail.ru>
2024-04-07 14:03:03 +0300

    кодварс решил

--- PythonSintacsis/5.py
+++ PythonSintacsis/5.py
@@ -17,22 +17,27 @@
 # lol2 = lol('Art', 22)
 # print(lol2._name('Arthur'))
 # print(lol._name())
+#https://www.codewars.com/kata/581214d54624a8232100005f/train/python 
 class codewars:    
     def matrix(self, array): 
         self.array = array
         resu = array
         i = 0
-        while len(resu) < i:
+        while len(resu) > i:
+            # x < 0 = 0
             if (resu[i][i]) < 0:
-                (resu[i][i]) = 0
+                resu[i][i] = 0
+            # x < 0 = 1
             else:
-                (resu[i][i]) = 1
+                resu[i][i] = 1
             i+=1
         return (resu)
            
-matem = codewars
-print(matem.matrix([[-1, 4, -5, -9, 3], 
-                    [6, -4, -7, 4, -5], 
-                    [3, 5, 4, -9, -1], 
-                    [1, 5, -7, -8, -9], 
-                    [-3, 2, 1, -5, 6]]))
+matem = codewars()
+
+for item in (matem.matrix([[-1, 4,  -5, -9,  3], 
+                    [6, -4, -7,  4, -5], 
+                    [3,  5,   4, -9, -1], 
+                    [1,  5,  -7, -8, -9], 
+                    [-3, 2,   1, -5,  6]])):
+    print(item)
