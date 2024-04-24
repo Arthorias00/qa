9df8ca9ee8d5c0fc0f33810e2af89a19593554b8
Arthur-QA-000 <ibatullin.97@mail.ru>
2024-04-24 12:59:27 +0300

    установил пайтест и проверяю

--- PythonSintacsis/selenium_test.py
+++ PythonSintacsis/selenium_test.py
@@ -5,7 +5,10 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def open_page():
+    browser.get('https://dev.1c-bitrix.ru/api_help/iblock/classes/ciblockelement/index.php')
+    bullet_car = browser.find_element(By.XPATH,"//*[contains(text(), 'Изменяет элемент.')]")
+    print(bullet_car.text)
 
-def test_open():
-     browser.get('https://dev.1c-bitrix.ru/api_help/iblock/classes/ciblockelement/index.php')
-time.sleep(15)
+def test_open_page():
+    open_page()
