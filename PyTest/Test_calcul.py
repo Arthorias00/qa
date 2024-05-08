7d840e120dc402262890e12be7e48459b6369735
Arthur-QA-000 <ibatullin.97@mail.ru>
2024-05-08 07:04:13 +0300

    fixture 2

--- PyTest/Test_calcul.py
+++ PyTest/Test_calcul.py
@@ -1,17 +1,29 @@
-
-
+import pytest
+@pytest.mark.regress
+@pytest.mark.skip(reason="Jira-123")
 def test_addition(calculator):
-    calculator.push('C')
+    #calculator.push('C') - в фикстуре
     calculator.push('4')
     calculator.push('+')
     calculator.push('7')
     result = calculator.push('=')
     assert result == "11"
 
+@pytest.mark.regres
 def test_substruction(calculator):
-    calculator.push('C')
+    #calculator.push('C') - в фикстуре
     calculator.push('7')
     calculator.push('-')
     calculator.push('4')
     result = calculator.push('=')
     assert result == "3"
+
+#тест проходит если упал, если он пройдет то будет ошибка
+@pytest.mark.xfail(strict=True)
+def test_first_error(calculator):
+    #calculator.push('C') - в фикстуре
+    calculator.push('4')
+    calculator.push('+')
+    calculator.push('7')
+    result = calculator.push('=')
+    assert result == "15"
--- PyTest/conftest.py
+++ PyTest/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from PythonSintacsis.cool_cal import Calculator
+#применяется к каждой функции
 @pytest.fixture(scope='function')
 def calculator():
     calc = Calculator()
-    return calc
+    #Как return но мы можем дальше действия выполнять
+    yield calc
+    calc.push('C')
